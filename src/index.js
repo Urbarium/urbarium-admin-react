442a1f2f5714dcaa745c588394f2f72ed10c5872
Emmanuel Mora <emmanuelmora05@gmail.com>
2019-02-09 00:49:01 -0600

    Add firestore credentials.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,14 @@ import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
 import FirebaseApp from './modules/FirebaseApp'
 import rootReducer, { initialState } from './reducers'
 
-const fbConfig = {};
+const fbConfig = {
+  apiKey: "AIzaSyDysVlFw_qKzvzmBxAs3jYwdWdI2qlmFS8",
+  authDomain: "urbarium-org.firebaseapp.com",
+  databaseURL: "https://urbarium-org.firebaseio.com",
+  projectId: "urbarium-org",
+  storageBucket: "urbarium-org.appspot.com",
+  messagingSenderId: "1043005680434"
+};
 
 // react-redux
 const rrfConfig = {
