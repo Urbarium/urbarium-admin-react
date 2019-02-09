2183a6f0b122a408712c257e08c838ce155e2aa3
Emmanuel Mora <emmanuelmora05@gmail.com>
2019-02-08 21:46:17 -0600

    Add Login Page grid.

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import ContentWrapper from '../components/ContentWrapper';
 import PageTitle from '../components/PageTitle';
 import LoginForm from '../components/LoginForm';
+import Page, { Grid, GridColumn } from '@atlaskit/page';
 
 export default class LoginPage extends Component {
   render() {
     return (
       <ContentWrapper>
         <PageTitle>Login</PageTitle>
-        <LoginForm />
+        <Page>
+          <Grid>
+            <GridColumn medium={3} />
+            <GridColumn medium={6}>
+              <LoginForm />
+            </GridColumn>
+            <GridColumn medium={3} />
+          </Grid>
+        </Page>
       </ContentWrapper>
     );
   }
