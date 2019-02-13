a5bd0e1bd7dba31831debe88522d2dff270b7308
Emmanuel Mora <emmanuelmora05@gmail.com>
2019-02-13 00:12:59 -0600

    Fix auth redirection.

--- src/components/AuthorizedPages.js
+++ src/components/AuthorizedPages.js
@@ -1,6 +1,6 @@
-import locationHelperBuilder from 'redux-auth-wrapper/history4/locationHelper';
-import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect'
-import history from './history'
+import locationHelperBuilder from 'redux-auth-wrapper/history3/locationHelper';
+import { connectedRouterRedirect } from 'redux-auth-wrapper/history3/redirect'
+import { routerActions } from 'react-router-redux'
 import LoginPage from '../pages/LoginPage';
 
 const locationHelper = locationHelperBuilder({});
@@ -9,14 +9,13 @@ export let UserIsAuthenticated = connectedRouterRedirect({
   wrapperDisplayName: 'UserIsAuthenticated',
   AuthenticatingComponent: LoginPage,
   allowRedirectBack: true,
-  redirectPath: (state, ownProps) =>
-    locationHelper.getRedirectQueryParam(ownProps) || '/login',
+  redirectPath: (state, ownProps) => '/login',
   authenticatingSelector: ({ firebase: { auth, profile, isInitializing } }) =>
     !auth.isLoaded || isInitializing === true,
   authenticatedSelector: ({ firebase: { auth } }) =>
     auth.isLoaded && !auth.isEmpty,
   redirectAction: newLoc => (dispatch) => {
-    history.push(newLoc); // or routerActions.replace
+    routerActions.replace
     dispatch({ type: 'UNAUTHED_REDIRECT' });
   },
 });
@@ -25,14 +24,13 @@ export let UserIsNotAuthenticated = connectedRouterRedirect({
   wrapperDisplayName: 'UserIsNotAuthenticated',
   AuthenticatingComponent: LoginPage,
   allowRedirectBack: false,
-  redirectPath: (state, ownProps) =>
-    locationHelper.getRedirectQueryParam(ownProps) || '/',
+  redirectPath: (state, ownProps) => '/',
   authenticatingSelector: ({ firebase: { auth, isInitializing } }) =>
     !auth.isLoaded || isInitializing === true,
   authenticatedSelector: ({ firebase: { auth } }) =>
     auth.isLoaded && auth.isEmpty,
   redirectAction: newLoc => (dispatch) => {
-    history.push(newLoc); // or routerActions.replace
+    routerActions.replace
     dispatch({ type: 'UNAUTHED_REDIRECT' });
   },
 });
