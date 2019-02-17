ef2c65149cc3a10065ebcbcc8ad224221228dd83
Emmanuel Mora <emmanuelmora05@gmail.com>
2019-02-17 12:36:56 -0600

    Remove Home description.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactLoading from "react-loading";
 import FullBackground from './FullBackground'
 
-
-
 const fullLoadingScreen = () => (
   <FullBackground centered>
     <ReactLoading type="bars" color="#fff" />
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import Button, { ButtonGroup } from '@atlaskit/button';
 import MainSection from '../components/MainSection';
 import ContentWrapper from '../components/ContentWrapper';
 import PageTitle from '../components/PageTitle';
@@ -12,14 +11,6 @@ class HomePage extends Component {
       <ContentWrapper>
         <PageTitle>Home</PageTitle>
         <MainSection />
-        <ButtonGroup>
-          <Button
-            appearance="primary"
-            onClick={this.context.showModal}
-            onClose={() => { }}
-          >Click to view Atlaskit modal</Button>
-          <Button onClick={this.context.addFlag}>click to view Atlaskit flag</Button>
-        </ButtonGroup>
       </ContentWrapper>
     );
   }
