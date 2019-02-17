e3510dc23ebd194dd4d9b32dcd20e7736369f1b2
Emmanuel Mora <emmanuelmora05@gmail.com>
2019-02-17 00:33:23 -0600

    Change icons.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import GlobalNavigation from '@atlaskit/global-navigation';
 import styled from 'styled-components';
 import { GroupHeading, Item, Section, Separator, Wordmark } from '@atlaskit/navigation-next';
 import { Route, Switch } from "react-router";
@@ -9,9 +8,17 @@ import { ToggleStateless } from "@atlaskit/toggle";
 import { ConnectedRouter } from 'connected-react-router'
 import { history } from '../../configureStore'
 
+import GlobalNavigation from '@atlaskit/global-navigation';
+import DashboardIcon from '@atlaskit/icon/glyph/dashboard';
+import PeopleGroupIcon from '@atlaskit/icon/glyph/people-group';
+import SettingsIcon from '@atlaskit/icon/glyph/settings';
+import { JiraIcon, JiraWordmark } from '@atlaskit/logo';
+
 import {
-  LayoutManager,
-  NavigationProvider
+  LayoutManagerWithViewController,
+  NavigationProvider,
+  ViewController,
+  withNavigationViewController,
 } from "@atlaskit/navigation-next";
 
 import {
@@ -24,29 +31,79 @@ import UpdateProfilePage from '../../pages/UpdateProfilePage';
 import LoginPage from '../../pages/LoginPage';
 import ProductNavigation from './ProductNavigation';
 
-class Navigation extends Component{
+const productHomeView = {
+  id: 'product/home',
+  type: 'product',
+  getItems: () => [
+    {
+      type: 'HeaderSection',
+      id: 'product/home:header',
+      items: [
+        {
+          type: 'Item',
+          text: 'Urbarium',
+          id: 'urbarium-wordmark',
+        },
+      ],
+    },
+    {
+      type: 'MenuSection',
+      id: 'product/home:menu',
+      items: [
+        {
+          type: 'Item',
+          id: 'dashboards',
+          before: DashboardIcon,
+          text: 'Dashboards',
+        },
+        { type: 'Item', 
+          id: 'users', 
+          before: PeopleGroupIcon, 
+          text: 'Users' },
+        {
+          type: 'Item',
+          id: 'settings',
+          before: SettingsIcon,
+          text: 'Settings',
+        }
+      ],
+    },
+  ],
+};
+
+class Navigation extends Component<{navigationViewController: ViewController}> {
+
+  componentDidMount() {
+    const { navigationViewController } = this.props;
+    navigationViewController.addView(productHomeView);
+    navigationViewController.setView(productHomeView.id);
+  }
 
   render() {
 
     return (
       <ConnectedRouter history={history}>
-        <NavigationProvider>
-          <LayoutManager
-            globalNavigation={DefaultGlobalNavigation}
-            productNavigation={ProductNavigation}
-          >
-            <div style={{ padding: 40 }}>
-              <Switch>
-                <Route path="/users" component={UpdateProfilePage} />
-                <Route path="/settings" component={UpdateProfilePage} />
-                <Route path="/" component={HomePage} />
-              </Switch>
-            </div>
-          </LayoutManager>
-        </NavigationProvider>
+        <LayoutManagerWithViewController
+          globalNavigation={DefaultGlobalNavigation}
+          productNavigation={ProductNavigation}
+        >
+          <div css={{ padding: 40 }}>
+            <Switch>
+              <Route path="/users" component={UpdateProfilePage} />
+              <Route path="/settings" component={UpdateProfilePage} />
+              <Route path="/" component={HomePage} />
+            </Switch>
+          </div>
+        </LayoutManagerWithViewController>
       </ConnectedRouter>
     )
   }
 }
 
-export default Navigation
+const AppWithNavigationViewController = withNavigationViewController(Navigation)
+
+export default () => (
+  <NavigationProvider>
+    <AppWithNavigationViewController />
+  </NavigationProvider>
+);
