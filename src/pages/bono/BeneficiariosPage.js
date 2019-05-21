21b3bc32b4e5e5617ba35f1a8d91c8a48def3c34
Emmanuel Mora <emmanuelmora05@gmail.com>
2019-05-21 00:17:38 -0600

    Add shared library 0.1.3

--- src/pages/bono/BeneficiariosPage.js
+++ src/pages/bono/BeneficiariosPage.js
@@ -4,12 +4,16 @@ import MainSection from '../../components/MainSection';
 import ContentWrapper from '../../components/ContentWrapper';
 import PageTitle from '../../components/PageTitle';
 
+// TODO: @richifg, check how to use a shared component.
+import { InputDropdown } from 'urbarium-shared-components'
+
 class BeneficiariosPage extends Component {
 
   render() {
     return (
       <ContentWrapper>
         <PageTitle>Beneficiarios</PageTitle>
+          <InputDropdown placeholder='Provincia' options={['San Jose', 'Alajuela', 'Heredia', 'Cartago', 'Limon', 'Puntarenas', 'Guancaste']}/>
         <MainSection />
       </ContentWrapper>
     );
