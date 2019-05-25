d68635f9ffde3048bc4263990acc569474f5c0b9
richifg <richifg@gmail.com>
2019-05-25 17:03:01 -0600

    Updated package to latest version of share-components

--- src/pages/bono/BeneficiariosPage.js
+++ src/pages/bono/BeneficiariosPage.js
@@ -1,11 +1,18 @@
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import MainSection from '../../components/MainSection';
 import ContentWrapper from '../../components/ContentWrapper';
 import PageTitle from '../../components/PageTitle';
 
-// TODO: @richifg, check how to use a shared component.
-// import { InputDropdown } from 'urbarium-shared-components'
+//import { LabeledInput } from 'urbarium-shared-components';
+
+// ubarium shared comps is now recognized but when reading its exports,
+// this project is not able to parse them. I get the following error:
+//    Module parse failed: Unexpected token (16:31)
+//    You may need an appropriate loader to handle this file type.
+// It happens no matter which file I export from shared-components
+
+// I read it has somenthing to do with babel presets / webpack, but I couldn't solve it.
+// good luck emma-sensei!
 
 class BeneficiariosPage extends Component {
 
@@ -13,8 +20,7 @@ class BeneficiariosPage extends Component {
     return (
       <ContentWrapper>
         <PageTitle>Beneficiarios</PageTitle>
-          {/* <InputDropdown placeholder='Provincia' options={['San Jose', 'Alajuela', 'Heredia', 'Cartago', 'Limon', 'Puntarenas', 'Guancaste']}/> */}
-        <MainSection />
+        <MainSection/>
       </ContentWrapper>
     );
   }
