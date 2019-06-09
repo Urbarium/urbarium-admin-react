3e4db15ac16c505f4c3d1ba5520f6bf3dba1e608
Emmanuel Mora <emmanuelmora05@gmail.com>
2019-06-09 12:35:26 -0600

    Remove styled-components references.

--- src/components/Urbarium/InputDropdown.js
+++ src/components/Urbarium/InputDropdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 import { primary } from '../../colors';
 import fonts from '../../fonts';
 import Arrow from './Arrow';
