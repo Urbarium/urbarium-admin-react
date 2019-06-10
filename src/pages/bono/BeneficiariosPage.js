2c5a50f55fa83d649f701b804b7cfae2a8e5a2b1
richifg <richifg@gmail.com>
2019-06-09 21:32:26 -0600

    Created initial BeneficiariosPage with linked dropdown test

--- src/pages/bono/BeneficiariosPage.js
+++ src/pages/bono/BeneficiariosPage.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
-import MainSection from '../../components/MainSection';
 import ContentWrapper from '../../components/ContentWrapper';
 import PageTitle from '../../components/PageTitle';
-
-//import { LabeledInput } from 'urbarium-shared-components';
-
-// ubarium shared comps is now recognized but when reading its exports,
-// this project is not able to parse them. I get the following error:
-//    Module parse failed: Unexpected token (16:31)
-//    You may need an appropriate loader to handle this file type.
-// It happens no matter which file I export from shared-components
-
-// I read it has somenthing to do with babel presets / webpack, but I couldn't solve it.
-// good luck emma-sensei!
+import Beneficiarios from '../../components/Urbarium/BeneficiariosSection';
+import Input from '../../components/Urbarium/LabeledInput';
+import DropdownGroup from '../../components/Urbarium/DropdownGroup';
+import Label from '../../components/Urbarium/Label';
+import { Row, Column } from '../../components/Structural/index';
 
 class BeneficiariosPage extends Component {
 
   render() {
+    const data = this.props.data
     return (
       <ContentWrapper>
-        <PageTitle>Beneficiarios</PageTitle>
-        <MainSection/>
+        <PageTitle>{data.title}</PageTitle>
+        <Column gap={2}>
+
+          <Beneficiarios data={data.beneficiarios}/>
+
+          <Column gap={1}>
+            <Label>Dirección</Label>
+            <DropdownGroup/>
+            <Input type="textarea" placeholder="Dirección exacta" height="100px" fill data={data.direccion}/>
+          </Column>
+
+          <Row columns={3}>
+            <Input type="textbox" label="Telefono" placeholder="0000 0000" data={data.telefono}/>
+            <Input type="textbox" label="Celular" placeholder="0000 0000" data={data.cedula}/>
+            {/* This below is ridiculous, gotta find a better way to align this*/ }
+            <div style={{width: "190px"}}/>
+          </Row>
+        </Column>
+
       </ContentWrapper>
     );
+  }
+}
+
+BeneficiariosPage.defaultProps = {
+  data: {
+    title: 'Beneficiarios',
+    beneficiarios: [
+      {
+        cedula: '',
+        nombre: '',
+        primer_apellido: '',
+        segundo_apellido: ''
+      }
+    ],
+    provincia: '',
+    canton: '',
+    distrito: '',
+    direccion: '',
+    telefono: '',
+    celular: '',
   }
 }
 
