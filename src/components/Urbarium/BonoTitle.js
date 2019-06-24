152519b083162746ba256660e2a11ca8b79e3763
richifg <richifg@gmail.com>
2019-06-24 00:48:08 -0600

    added bonoTitle comp

---
+++ src/components/Urbarium/BonoTitle.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Label from './Label';
+import { Row, Column } from '../Structural/index';
+import { colors } from './urbarium-theme';
+
+const titleTheme = {
+  label_color: '#23262',
+  label_fontSize: '22px',
+  label_fontWeight: 'bold',
+};
+
+const numberTheme = {
+  label_color: colors.primary,
+  label_fontSize: '21px',
+  label_fontWeight: 'bold',
+};
+
+const headerNameTheme = {
+  label_color: '#A0AAB4',
+  label_fontSize: '10.45px',
+  label_fontWeight: 'normal',
+};
+
+const headerValueTheme = {
+  label_color: colors.primary,
+  label_fontSize: '15px',
+  label_fontWeight: 'bold',
+};
+
+export default ({
+  nombre = 'Default',
+  cedula = '12345678',
+  numero = '12345',
+  monto = '',
+  modificacion = '',
+  creacion = '00-00-00',
+}) => (
+  <Row>
+    <Column>
+      <Label theme={titleTheme}>{`${nombre}-${cedula}`}</Label>
+      <Label theme={numberTheme}>{`#${numero}`}</Label>
+    </Column>
+
+    <Row justify="end" gap={50}>
+      <Column justify="end">
+        <Label theme={headerNameTheme}>MONTO DEL BONO</Label>
+        <Label theme={headerValueTheme}>{monto}</Label>
+      </Column>
+      <Column justify="end">
+        <Label theme={headerNameTheme}>ÚLTIMA MODIFICACIÓN</Label>
+        <Label theme={headerValueTheme}>{modificacion}</Label>
+      </Column>
+      <Column justify="end">
+        <Label theme={headerNameTheme}>FECHA CREACIÓN</Label>
+        <Label theme={headerValueTheme}>{creacion}</Label>
+      </Column>
+    </Row>
+  </Row>
+);
