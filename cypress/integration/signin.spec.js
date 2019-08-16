0ab70391ee699fc340d884ed44a71a85453626a1
Emmanuel Mora <emmanuelmora05@gmail.com>
2019-08-16 00:44:09 -0600

    Add cypress fixtures.

--- cypress/integration/signin.spec.js
+++ cypress/integration/signin.spec.js
@@ -4,37 +4,54 @@
 /// <reference types="Cypress" />
 
 context('In / path', () => {
-  beforeEach(() => {
-    indexedDB.deleteDatabase('firebaseLocalStorageDb');
+  before(function () {
     cy.fixture('profile').as('profileData');
+    cy.fixture('selectors').then((selectors) => {
+      this.selectors = selectors.login;
+    });
+    cy.fixture('messages').then((messages) => {
+      this.messages = messages.login;
+      this.messages.analyticsTitle = messages.analytics.title;
+    });
+  });
+
+  beforeEach(function () {
+    indexedDB.deleteDatabase('firebaseLocalStorageDb');
     cy.visit('/');
   });
 
-  describe('when rendered', () => {
-    it('has a correct title', () => {
-      cy.get('h1').should('have.text', "Urbarium Login");
+  describe('when rendered', function () {
+    it('has a correct title', function () {
+      const { title } = this.messages;
+      cy.get('h1').should('have.text', title);
     });
 
-    it('has a sign in button', () => {
-      cy.get('button[type=submit]').should('have.text', "Submit");
+    it('has a sign in button', function () {
+      const { submitBtn } = this.selectors;
+      const { submitLabel } = this.messages;
+      cy.get(submitBtn).should('have.text', submitLabel);
     });
 
-    it('has a email input', () => {
-      cy.get('input[name=email]').should('exist');
+    it('has a email input', function () {
+      const { emailInput } = this.selectors;
+      cy.get(emailInput).should('exist');
     });
 
-    it('has a password input', () => {
-      cy.get('input[name=password]').should('exist');
+    it('has a password input', function () {
+      const { passwordInput } = this.selectors;
+      cy.get(passwordInput).should('exist');
     });
   });
 
   describe('with valid credentials', function () {
     it('signs in and renders Analytics Page', function () {
       const { email, password } = this.profileData;
-      cy.get('input[name=email]').type(email);
-      cy.get('input[name=password]').type(password);
-      cy.get('button[type=submit]').click();
-      cy.get('h1').should('have.text', 'Analytics');
+      const { emailInput, passwordInput, submitBtn } = this.selectors;
+      const { analyticsTitle } = this.messages;
+      cy.get(emailInput).type(email);
+      cy.get(passwordInput).type(password);
+      cy.get(submitBtn).click();
+      cy.get('h1').should('have.text', analyticsTitle);
     });
   });
 
@@ -42,11 +59,18 @@ context('In / path', () => {
     it('shows error', function () {
       const { email } = this.profileData;
       const password = 'bad password';
-      cy.get('input[name=email]').type(email);
-      cy.get('input[name=password]').type(password);
-      cy.get('button[type=submit]').click();
-      cy.get('h1').should('have.text', "Urbarium Login");
-      cy.get('form').contains("The password is invalid or the user does not have a password.");
+      const {
+        emailInput,
+        passwordInput,
+        submitBtn,
+        signInForm,
+      } = this.selectors;
+      const { title, badPassword } = this.messages;
+      cy.get(emailInput).type(email);
+      cy.get(passwordInput).type(password);
+      cy.get(submitBtn).click();
+      cy.get('h1').should('have.text', title);
+      cy.get(signInForm).contains(badPassword);
     });
   });
 
@@ -54,11 +78,18 @@ context('In / path', () => {
     it('shows error', function () {
       const email = 'bademail@example.com';
       const password = 'bad password';
-      cy.get('input[name=email]').type(email);
-      cy.get('input[name=password]').type(password);
-      cy.get('button[type=submit]').click();
-      cy.get('h1').should('have.text', "Urbarium Login");
-      cy.get('form').contains("There is no user record corresponding to this identifier. The user may have been deleted.");
+      const {
+        emailInput,
+        passwordInput,
+        submitBtn,
+        signInForm,
+      } = this.selectors;
+      const { title, badEmail } = this.messages;
+      cy.get(emailInput).type(email);
+      cy.get(passwordInput).type(password);
+      cy.get(submitBtn).click();
+      cy.get('h1').should('have.text', title);
+      cy.get(signInForm).contains(badEmail);
     });
   });
 });
